5bf52a60ad9107df6fd35c183d0d8236575c0c3b
Martin Lanser <martinlanser@gmail.com>
2023-12-16 16:24:16 -0500

    minor code tweaks

--- src/f451-cli-ui/cli_ui.py
+++ src/f451-cli-ui/cli_ui.py
@@ -26,11 +26,11 @@ from rich.table import Table
 from sparklines import sparklines
 
 import f451_common.common as f451Common
-import f451_sensehat.sensehat_data as f451SenseData
+# import f451_sensehat.sensehat_data as f451SenseData
 
 
 __all__ = [
-    # 'Cloud',
+    'BaseUI',
     # 'CloudError',
     # 'KWD_AIO_ID',
     # 'KWD_AIO_KEY',
@@ -104,153 +104,10 @@ class Logo:
         return f"{type(self).__name__}(plain={self._plain!r})"
 
 
-def render_table(data, labelsOnly=False):
-    """Make a new table
-
-    This is a beefy function and (re-)renders the whole table
-    on each update so that we get that real-time update feel.
-
-    Args:
-        data:
-            'list' of data rows, each with a specific data set render
-        labelsOnly:
-            'bool' if 'True' then we only render labels and no data
-
-    Returns:
-        'Table' with data
-    """
-
-    def _prep_currval_str(data, labelsOnly=False):
-        """Prep string for displaying current/last data point
-
-        This is a formatted string with a data value and unit of
-        measure. The largest value will be 4 digits + 2 decimals.
-        We also want values to be right-justfied and align on the
-        decimal point.
-
-        -->|        |<--
-           |12345678|
-        ---|--------|---
-           |1,234.56|      <- Need min 8 char width for data values
-           |    1.23|
-
-        NOTE: We display '--' if value is 'None' and status is 'ok' 
-              as that represents a 'missing' value. But we display 
-              'ERROR' if value is 'None' and status is not 'ok' as
-              that indicates an invalid value. Both cases are shown
-              as gaps in sparkline graph.   
-        
-        Args:
-            data:
-                'dict' with data point value and attributes
-            labelsOnly:
-                'bool' if 'True' then we do not generate 'current value' string
-
-        Returns:
-            'Text' object with formatted 'current value'
-        """
-        text = Text()
-        dirChar = CHAR_DIR_DEF
-
-        if labelsOnly or (data['dataPt'] is None and data['dataPtOK']):
-            text.append(f"{dirChar} {VAL_BLANK_STR:>8} {data['unit']}", COLOR_DEF)
-        elif data['dataPt'] is None and not data['dataPtOK']:
-            text.append(f"{dirChar} {VAL_ERROR_STR:>8}", COLOR_ERROR)
-        else:
-            if data['dataPtDelta'] > 0:
-                dirChar = CHAR_DIR_UP
-            elif data['dataPtDelta'] < 0:
-                dirChar = CHAR_DIR_DWN
-            else:
-                dirChar = CHAR_DIR_EQ
-
-            text.append(
-                f"{dirChar} {data['dataPt']:>8,.2f} {data['unit']}",
-                data['dataPtColor']
-            )
-
-        return text
-
-    def _prep_sparkline_str(data, labelsOnly):
-        """Prep sparkline graph string
-
-        NOTE: 'sparklines' library will return string with ANSI color 
-              codes when used with 'termcolors' library.
-
-        Args:
-            data:
-                'dict' with data point value and attributes
-            labelsOnly:
-                'bool' if 'True' then we do not generate 'current value' string
-
-        Returns:
-            'Text' object with formatted 'current value'
-        """
-        if labelsOnly or not data['sparkData']:
-            return ''
-        else:
-            return Text.from_ansi(
-                sparklines(
-                    data['sparkData'], 
-                    emph=data['sparkColors'], 
-                    num_lines=1, 
-                    minimum=data['sparkMinMax'][0],
-                    maximum=data['sparkMinMax'][1]
-                )[-1], 
-                justify="center"
-            )
-
-    # Build a table
-    table = Table(
-        show_header=True,
-        show_footer=False,
-        show_edge=True,
-        show_lines=True,
-        expand=True,
-        box=box.SQUARE_DOUBLE_HEAD,
-    )
-    table.add_column(
-        Text('Description', justify='center'),
-        ratio=1,
-        width=12,
-        no_wrap=True,
-        overflow='crop',
-    )
-    table.add_column(
-        Text('Current', justify='center'), 
-        ratio=1, 
-        width=16, 
-        no_wrap=True, 
-        overflow='crop'
-    )
-    table.add_column(
-        Text('History', justify='center'),
-        ratio=4,
-        min_width=12,
-        no_wrap=True,
-        overflow='crop',
-    )
-
-    # Render rows with/without data
-    if data:
-        for row in data:
-            table.add_row(
-                row['label'],                           # 1st col: label
-                _prep_currval_str(row, labelsOnly),     # 2nd col: current value
-                _prep_sparkline_str(row, labelsOnly)    # 3rd col: sparkline
-            )
-    else:
-        table.add_row('', '', '')
-        table.add_row('', '', '')
-        table.add_row('', '', '')
-
-    return table
-
-
 # =========================================================
 #                     M A I N   C L A S S
 # =========================================================
-class SenseMonUI:
+class BaseUI:
     def __init__(self):
         self._console = Console()
         self._layout = Layout()
@@ -295,6 +152,23 @@ class SenseMonUI:
         return time.strftime(timeFmtStr, time.localtime(t) if self.showLocal else time.gmtime(t))
 
     @staticmethod
+    def _make_footer(appName, width, customColors=None):
+        """Create 'footer' object"""
+        colorMap = f451Common.get_tri_colors(customColors)
+        footer = Text()
+
+        # Assemble colorful legend
+        footer.append('  LOW ', colorMap.low)
+        footer.append('NORMAL ', colorMap.normal)
+        footer.append('HIGH', colorMap.high)
+
+        # Add app name and version, and push to the right
+        footer.append(appName.rjust(width - len(str(footer)) - 2))
+        footer.end = ''
+
+        return footer
+
+    @staticmethod
     def _make_progressbar(console, refreshRate=2):
         """Initialize progress bar object"""
         return Progress(
@@ -312,6 +186,149 @@ class SenseMonUI:
         msg = msg if msg is not None else STATUS_LBL_WAIT
         return Status(msg, console=console, spinner='dots')
 
+    @staticmethod
+    def _render_table(data, labelsOnly=False):
+        """Make a new table
+
+        This is a beefy function and (re-)renders the whole table
+        on each update so that we get that real-time update feel.
+
+        Args:
+            data:
+                'list' of data rows, each with a specific data set render
+            labelsOnly:
+                'bool' if 'True' then we only render labels and no data
+
+        Returns:
+            'Table' with data
+        """
+
+        def _prep_currval_str(data, labelsOnly=False):
+            """Prep string for displaying current/last data point
+
+            This is a formatted string with a data value and unit of
+            measure. The largest value will be 4 digits + 2 decimals.
+            We also want values to be right-justfied and align on the
+            decimal point.
+
+            -->|        |<--
+            |12345678|
+            ---|--------|---
+            |1,234.56|      <- Need min 8 char width for data values
+            |    1.23|
+
+            NOTE: We display '--' if value is 'None' and status is 'ok' 
+                as that represents a 'missing' value. But we display 
+                'ERROR' if value is 'None' and status is not 'ok' as
+                that indicates an invalid value. Both cases are shown
+                as gaps in sparkline graph.   
+            
+            Args:
+                data:
+                    'dict' with data point value and attributes
+                labelsOnly:
+                    'bool' if 'True' then we do not generate 'current value' string
+
+            Returns:
+                'Text' object with formatted 'current value'
+            """
+            text = Text()
+            dirChar = CHAR_DIR_DEF
+
+            if labelsOnly or (data['dataPt'] is None and data['dataPtOK']):
+                text.append(f"{dirChar} {VAL_BLANK_STR:>8} {data['unit']}", COLOR_DEF)
+            elif data['dataPt'] is None and not data['dataPtOK']:
+                text.append(f"{dirChar} {VAL_ERROR_STR:>8}", COLOR_ERROR)
+            else:
+                if data['dataPtDelta'] > 0:
+                    dirChar = CHAR_DIR_UP
+                elif data['dataPtDelta'] < 0:
+                    dirChar = CHAR_DIR_DWN
+                else:
+                    dirChar = CHAR_DIR_EQ
+
+                text.append(
+                    f"{dirChar} {data['dataPt']:>8,.2f} {data['unit']}",
+                    data['dataPtColor']
+                )
+
+            return text
+
+        def _prep_sparkline_str(data, labelsOnly):
+            """Prep sparkline graph string
+
+            NOTE: 'sparklines' library will return string with ANSI color 
+                codes when used with 'termcolors' library.
+
+            Args:
+                data:
+                    'dict' with data point value and attributes
+                labelsOnly:
+                    'bool' if 'True' then we do not generate 'current value' string
+
+            Returns:
+                'Text' object with formatted 'current value'
+            """
+            if labelsOnly or not data['sparkData']:
+                return ''
+            else:
+                return Text.from_ansi(
+                    sparklines(
+                        data['sparkData'], 
+                        emph=data['sparkColors'], 
+                        num_lines=1, 
+                        minimum=data['sparkMinMax'][0],
+                        maximum=data['sparkMinMax'][1]
+                    )[-1], 
+                    justify="center"
+                )
+
+        # Build a table
+        table = Table(
+            show_header=True,
+            show_footer=False,
+            show_edge=True,
+            show_lines=True,
+            expand=True,
+            box=box.SQUARE_DOUBLE_HEAD,
+        )
+        table.add_column(
+            Text('Description', justify='center'),
+            # ratio=1,
+            width=12,
+            no_wrap=True,
+            overflow='crop',
+        )
+        table.add_column(
+            Text('Current', justify='center'), 
+            # ratio=1, 
+            width=16, 
+            no_wrap=True, 
+            overflow='crop'
+        )
+        table.add_column(
+            Text('History', justify='center'),
+            # ratio=4,
+            min_width=12,
+            no_wrap=True,
+            overflow='crop',
+        )
+
+        # Render rows with/without data
+        if data:
+            for row in data:
+                table.add_row(
+                    row['label'],                           # 1st col: label
+                    _prep_currval_str(row, labelsOnly),     # 2nd col: current value
+                    _prep_sparkline_str(row, labelsOnly)    # 3rd col: sparkline
+                )
+        else:
+            table.add_row('', '', '')
+            table.add_row('', '', '')
+            table.add_row('', '', '')
+
+        return table
+
     def initialize(self, appNameLong, appNameShort, appVer, dataRows, enable=True):
         """Initialize main UI
         
@@ -328,21 +345,6 @@ class SenseMonUI:
             dataRows: table rows with labels
             enable: 
         """
-        def _footer(appName, conWidth):
-            """Create 'footer' object"""
-            footer = Text()
-
-            # Assemble colorful legend
-            footer.append('  LOW ', f451SenseData.COLOR_MAP[f451SenseData.COLOR_LOW])
-            footer.append('NORMAL ', f451SenseData.COLOR_MAP[f451SenseData.COLOR_NORM])
-            footer.append('HIGH', f451SenseData.COLOR_MAP[f451SenseData.COLOR_HIGH])
-
-            # Add app name and version, and push to the right
-            footer.append(appName.rjust(conWidth - len(str(footer)) - 2))
-            footer.end = ''
-
-            return footer
-
         # Get dimensions of screen/console 
         # console = Console()
         conWidth, conHeight = self._console.size
@@ -397,13 +399,13 @@ class SenseMonUI:
         self._layout['actNextUpld'].update(Text(f'{self.statusLblNext}--:--:--'))
         self._layout['actLastUpld'].update(Text(f'{self.statusLblLast}--:--:--'))
         self._layout['actNumUpld'].update(Text(f'{self.statusLblTotUpld}-'))
-        self._layout['actCurrent'].update(SenseMonUI._make_statusbar(self._console))
+        self._layout['actCurrent'].update(BaseUI._make_statusbar(self._console))
 
         # Display main row with data table
-        self._layout['main'].update(render_table(dataRows, True))
+        self._layout['main'].update(BaseUI._render_table(dataRows, True))
 
         # Display footer row
-        self._layout['footer'].update(_footer(logo.plain, conWidth))
+        self._layout['footer'].update(BaseUI._make_footer(logo.plain, conWidth))
 
         # Update properties for this object ... and then we're done
         self._conWidth = conWidth
@@ -413,7 +415,7 @@ class SenseMonUI:
 
     def update_data(self, data):
         if self._active:
-            self._layout['main'].update(render_table(data))
+            self._layout['main'].update(BaseUI._render_table(data))
 
     def update_upload_num(self, num, maxNum=0):
         if self._active:
